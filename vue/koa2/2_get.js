acc3a645817ba0c03211c77e2c07b4850399754e
haolushan5253 <2587051793@qq.com>
2019-02-17 17:00:10 +0800

    koa的get与post

--- vue/koa2/2_get.js
+++ vue/koa2/2_get.js
@@ -1,9 +1,17 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx =>{
-    
-});
+// app.use( ctx => {
+//     let url = ctx.url;
+//     let query = ctx.query; // 对象
+//     let queryString = ctx.querystring; // 字符串
+
+//     ctx.body = {
+//         url,
+//         query,
+//         queryString
+//     };
+// });
 
 app.listen(3000,()=>{
     console.log('服务开启成功');
